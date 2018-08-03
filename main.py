e65ef025f56c4983219d4b18d545cb37f9fcb9e8
Apon Shahriar <apon.ruet@gmail.com>
2018-08-03 18:07:04 +0600

    dictionary name corrected

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def list_to_dict(inp_list):
     inp_list_len=len(inp_list)
     result_dict={}
     for i in range(inp_list_len):
-        if inp_list[i] in dict_mag:
+        if inp_list[i] in result_dict:
             result_dict[inp_list[i]]=result_dict[inp_list[i]]+1
         else:
             result_dict[inp_list[i]]=1
